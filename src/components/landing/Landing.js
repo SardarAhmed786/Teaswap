e4a44d1d63502e927436f7b5679ebe784bf45c35
sardarahmed786 <sardarahmadnaseem@gmail.com>
2024-03-19 19:39:12 +0500

    changes

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -34,7 +34,7 @@ function Landing() {
       <Tokenism />
       <Security />
       <Bridge />
-      <Partners />
+      {/* <Partners /> */}
       <Defi />
       {/* <News /> */}
       <Footer />
